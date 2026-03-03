c6e6ba65dd9423c6df689a529b39b52d4eb5bd77
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-03-03 11:46:42 -0800

    fixed fetch again

--- scripts/DataService.js
+++ scripts/DataService.js
@@ -1,8 +1,71 @@
-const apiData = async () => {
-    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net");
+const apiData1 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/AddingTwoNumbers/AddingTwoNumbers");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData2 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/AskingQuestions/AskingQuestions");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData3 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/GuessIt/GuessIt");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData4 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/HelloWorld/HelloWorld");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData5 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/MadLib/MadLib");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData6 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/Magic8Ball/Magic8Ball");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData7 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/OddorEven/OddorEven");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData8 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/RestaurantPicker/RestaurantPicker");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData9 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/ReverseItAlphanumeric/ReverseItAlphanumeric");
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+}
+const apiData10 = async () => {
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/ReverseItNumbers/ReverseItNumbers");
     const data = await response.json();
     console.log(data[0]);
     return data[0];
 }
 
-apiData();
+apiData1();
+apiData2();
+apiData3();
+apiData4();
+apiData5();
+apiData6();
+apiData7();
+apiData8();
+apiData9();
+apiData10();
