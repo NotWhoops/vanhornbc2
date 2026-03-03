69f2152686cf0948f15a98aa46657060838801bd
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-03-03 11:12:33 -0800

    fixed fetch

--- scripts/DataService.js
+++ scripts/DataService.js
@@ -1,5 +1,5 @@
 const apiData = async () => {
-    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net/AddingTwoNumbers/AddingTwoNumbers");
+    const response = await fetch("https://allforoneapi-hhdrdah6b0hgfgdz.westus3-01.azurewebsites.net");
     const data = await response.json();
     console.log(data[0]);
     return data[0];
